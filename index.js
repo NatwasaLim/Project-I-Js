7e497b117790a16f6cb1e345d0e86451bcdd42bd
NatwasaLim <32430994+NatwasaLim@users.noreply.github.com>
2017-10-23 00:23:26 +0700

    change footer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(function(){
     $("#getdata").click(function(){
         var url = "http://localhost:3000/posts";
@@ -11,18 +11,4 @@ $(function(){
             }
         });
     });
-=======
-$(function(){
-    $("#getdata").click(function(){
-        var url = "http://localhost:3000/posts";
-        $.get(url, function(data){
-            console.log(data);  
-            var template = $('#template').html();
-            for(var i=0;i<data.length;i++){
-                var rendered = Mustache.render(template, data[i]);
-                $("#posts").append(rendered);
-            }
-        });
-    });
->>>>>>> 424a37a71af4958225af2048ac72d85911555116
 });
